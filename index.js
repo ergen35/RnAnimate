26d8b2ab49f2822c7535c4e1cc953efbd3352689
ergen35 <ienoverse@gmail.com>
2024-12-27 11:34:01 +0100

    Testing Reanimated 3 & Moti

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+function AppRoot() {
+    return (
+        <GestureHandlerRootView>
+            <App />
+        </GestureHandlerRootView>
+    )
+}
+
+AppRegistry.registerComponent(appName, () => AppRoot);
